8f0d6532341a402ff799ebbf9727d285188da653
Zhang, Juanyong <juanyong.zhang@chpmail.com>
2017-02-16 10:31:17 -0500

    adding aspect search

--- src/main/groovy/io/tronbot/dc/domain/Business.groovy
+++ src/main/groovy/io/tronbot/dc/domain/Business.groovy
@@ -4,6 +4,8 @@ import javax.persistence.Access
 import javax.persistence.AccessType
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
@@ -13,6 +15,7 @@ import javax.persistence.Table
 import io.tronbot.dc.common.json.ArrayToSingleInterpreter
 import io.tronbot.dc.common.json.ArrayToStringInterpreter
 import io.tronbot.dc.common.json.JsonPathField
+import io.tronbot.dc.domain.Business.Type
 
 
 /**
@@ -64,4 +67,150 @@ public class Business{
 	@JsonPathField('$.result.geometry.location.lng')
 	Double longitude
 	Date timestamp = new Date()
+	@Enumerated(EnumType.STRING)
+	Type type;
+
+	enum Type{
+		accounting,
+		airport,
+		amusement_park,
+		aquarium,
+		art_gallery,
+		atm,
+		bakery,
+		bank,
+		bar,
+		beauty_salon,
+		bicycle_store,
+		book_store,
+		bowling_alley,
+		bus_station,
+		cafe,
+		campground,
+		car_dealer,
+		car_rental,
+		car_repair,
+		car_wash,
+		casino,
+		cemetery,
+		church,
+		city_hall,
+		clothing_store,
+		convenience_store,
+		courthouse,
+		dentist,
+		department_store,
+		doctor,
+		electrician,
+		electronics_store,
+		embassy,
+		establishment,
+		finance,
+		fire_station,
+		florist,
+		food,
+		funeral_home,
+		furniture_store,
+		gas_station,
+		general_contractor,
+		grocery_or_supermarket,
+		gym,
+		hair_care,
+		hardware_store,
+		health,
+		hindu_temple,
+		home_goods_store,
+		hospital,
+		insurance_agency,
+		jewelry_store,
+		laundry,
+		lawyer,
+		library,
+		liquor_store,
+		local_government_office,
+		locksmith,
+		lodging,
+		meal_delivery,
+		meal_takeaway,
+		mosque,
+		movie_rental,
+		movie_theater,
+		moving_company,
+		museum,
+		night_club,
+		painter,
+		park,
+		parking,
+		pet_store,
+		pharmacy,
+		physiotherapist,
+		place_of_worship,
+		plumber,
+		police,
+		post_office,
+		real_estate_agency,
+		restaurant,
+		roofing_contractor,
+		rv_park,
+		school,
+		shoe_store,
+		shopping_mall,
+		spa,
+		stadium,
+		storage,
+		store,
+		subway_station,
+		synagogue,
+		taxi_stand,
+		train_station,
+		transit_station,
+		travel_agency,
+		university,
+		veterinary_care,
+		zoo,
+		administrative_area_level_1,
+		administrative_area_level_2,
+		administrative_area_level_3,
+		administrative_area_level_4,
+		administrative_area_level_5,
+		colloquial_area,
+		country,
+		floor,
+		geocode,
+		intersection,
+		locality,
+		natural_feature,
+		neighborhood,
+		political,
+		point_of_interest,
+		post_box,
+		postal_code,
+		postal_code_prefix,
+		postal_code_suffix,
+		postal_town,
+		premise,
+		room,
+		route,
+		street_address,
+		street_number,
+		sublocality,
+		sublocality_level_4,
+		sublocality_level_5,
+		sublocality_level_3,
+		sublocality_level_2,
+		sublocality_level_1,
+		subpremise
+
+		public static Type valueOfIgnoreCase(String key){
+			if(key){
+				return Type.values().findResult  {
+					it.name().equalsIgnoreCase(key) ? it : null
+				}
+			}
+			return null;
+		}
+	}
 }
+
+
+
--- src/main/groovy/io/tronbot/dc/service/ReconciliationService.groovy
+++ src/main/groovy/io/tronbot/dc/service/ReconciliationService.groovy
@@ -7,9 +7,10 @@ import com.jayway.jsonpath.PathNotFoundException
 
 import groovy.util.logging.Log4j
 import io.tronbot.dc.client.GooglePlacesClient
-import io.tronbot.dc.dao.BusinessRepository
 import io.tronbot.dc.common.json.JsonPathReflector
+import io.tronbot.dc.dao.BusinessRepository
 import io.tronbot.dc.domain.Business
+import io.tronbot.dc.domain.Business.Type
 
 /**
  * @author <a href="mailto:juanyong.zhang@gmail.com">Juanyong Zhang</a> 
@@ -22,18 +23,28 @@ public class ReconciliationService{
 	private final JsonPathReflector json
 	private final BusinessRepository businessRepository
 
-
 	ReconciliationService(GooglePlacesClient client, JsonPathReflector json, BusinessRepository businessRepository){
 		this.client = client
 		this.json = json
 		this.businessRepository = businessRepository
 	}
 
+	
+	/**
+	 * 
+	 * @param keywords - comma separated string, business name, address... 
+	 * @return
+	 */
 	public Business queryBusiness(String keywords){
+		return queryWithType(guessBusinessType(keywords), keywords)
+	}
+	
+	public Business queryWithType(Type businessType, String keywords){
 		Business result = null
 		String resp = query(keywords)
 		if(StringUtils.isNotBlank(resp)){
 			result = json.from(query(keywords), new Business())
+			result.setType(businessType)
 			//FIXME MOVE TO KAFKA - Non-blocking way
 			if(!businessRepository.findByPlaceId(result.getPlaceId())){
 				businessRepository.save(result)
@@ -41,11 +52,16 @@ public class ReconciliationService{
 		}
 		return result
 	}
-
+	
+	
+	private Type guessBusinessType(String q){
+		String k = !q ?:  StringUtils.substring(q, 0, q.indexOf(','))
+		return Type.valueOfIgnoreCase(k)
+	}
 
 	public String query(String keywords){
 		keywords = keywords.toLowerCase().replace('\t', ' ').replaceAll('null', '').replaceAll('\\W -,.&', '').trim().replaceAll(' +', ' ')
-		String place = null;
+		String place = null
 		try {
 			String placeId = json.read(client.query(keywords), '$.results[0].place_id')
 			place = client.detail(placeId)
